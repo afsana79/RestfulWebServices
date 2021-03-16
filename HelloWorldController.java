69b59f6fac97d60e9deb19a00f1ba02d1e6582a1
afsana79 <74939522+afsana79@users.noreply.github.com>
2021-03-16 09:26:50 -0400

    Add files via upload
    Implementing Internationalization

--- HelloWorldController.java
+++ HelloWorldController.java
@@ -1,7 +1,13 @@
 package com.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +15,9 @@ public class HelloWorldController {
 	//uri - /helloWorld
 	//method - helloWorld
 	//GET
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	@GetMapping( path= "/hello-world")
 	public String helloWorld() {		
@@ -25,5 +34,11 @@ public class HelloWorldController {
 		return new HelloWorldBean( String.format("Hello World, %s", name) );
 		
 	}
+	
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("good.morning.message", null ,LocaleContextHolder.getLocale() );		
+	}
+	
 			
 }
